62e42aadeda1f64a287d4fed30d14fcc3142b2c4
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2025-11-24 18:49:57 +0000

    Update install-gcloud.sh

--- install-gcloud.sh
+++ install-gcloud.sh
@@ -5,7 +5,7 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-apt-get update && apt-get install apt-transport-https ca-certificates gnupg curl
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get update && sudo apt-get install google-cloud-cli
+apt-get update && apt-get install -y apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get update && apt-get install google-cloud-cli
