e5d3825cbaf073ab23e1f79584ca58eeac9bf8eb
Cameron McDermott <cameron_mcdermott@yahoo.co.uk>
2025-11-24 18:47:54 +0000

    Create install-gcloud.sh

---
+++ install-gcloud.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$(id -u)" != "0" ]; then
+  echo "Run with root"
+  exit 1
+fi
+
+apt-get update && apt-get install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install google-cloud-cli
